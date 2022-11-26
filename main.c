946fa3acd8413c62fc512a3f1f50f33488a4c932
Oleg Sirenko <spec_oleg@mail.ru>
2022-11-26 13:45:30 +0300

    Lab 4, task 3 ready

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "math.h"
 #include <stdlib.h> // для рандома
 
-int main()
+/*int main()
 {
     int n; // кол-во элементов массива
     int sum = 0;
@@ -39,5 +39,47 @@ int main()
 
     printf("Сумма положительных равна = %d\n", sum);
     printf("Сумма до минимума равна = %d\n", sum_for_min);
+    return 0;
+}
+
+*/
+
+int main(){
+    int n; // кол-во элементов массива
+    printf("Введите кол-во элементов массива:\n");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i=0; i<n; i++){ // генерация массива случайных чисел
+        arr[i] = 5 + rand() % (21);; // генерация случайного числа от 5 до 25
+        printf("%d\t", arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    int index_min = 0;
+    int index_max = 0;
+
+    // находим минимум и максимум
+    for(int i=0; i<n; i++){
+        if (max<arr[i]){
+            max=arr[i];
+            index_max = i;
+        }
+        if (min>arr[i]){
+            min=arr[i];
+            index_min = i;
+        }
+    }
+    printf("\n\tmax = %d на месте %d\n\tmin = %d на месте %d\n ", max,index_max, min, index_min);
+
+    // меняем местами
+    arr[index_min] = max;
+    arr[index_max] = min;
+
+    printf("Новый массив выглядит так:\n");
+    for(int i=0;i<n;i++){
+        printf("%d\t", arr[i]);
+    }
     return 0;
 }
